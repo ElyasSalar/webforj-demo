3827b20ee935a6b436c4cdfe7c25e1d58ad63848
Elyas <elyassalar8@gmail.com>
2024-11-21 21:02:13 +0300

    clean the main app

--- src/main/resources/script.js
+++
@@ -1,6 +0,0 @@
-const addonsScript = document.createElement("script");
-addonsScript.setAttribute("src", "/files/basis/addons-components/dwc-addons.esm.js");
-addonsScript.setAttribute("type", "module");
-
-const head = document.getElementsByTagName("head");
-document.head.appendChild(addonsScript);
