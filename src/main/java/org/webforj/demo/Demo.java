cb169e0f04692bc324cc66267cf09942a3613cf3
Elyas <elyassalar8@gmail.com>
2025-03-20 13:37:48 +0300

    feat: update the deployment to war file

--- src/main/java/org/webforj/demo/Demo.java
+++ src/main/java/org/webforj/demo/Demo.java
@@ -1,14 +1,13 @@
 package org.webforj.demo;
 
 import com.webforj.App;
+import com.webforj.annotation.*;
 import com.webforj.component.window.Frame;
 import com.webforj.exceptions.WebforjException;
 
-
+@AppAttribute(name = "lang", value = "en")
+@Routify(packages = "org.webforj.views")
+@AppTitle("Demo")
+@AppEntry()
 public class Demo extends App {
-  @Override
-  public void run() throws WebforjException {
-    Frame window = new Frame();
-
-  }
 }
---
+++ src/main/java/org/webforj/views/HomeView.java
@@ -0,0 +1,9 @@
+package org.webforj.views;
+
+import com.webforj.component.Composite;
+import com.webforj.component.html.elements.Div;
+import com.webforj.router.annotation.Route;
+
+@Route("/")
+public class HomeView extends Composite<Div> {
+}
