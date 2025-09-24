96863568b65aea5e1c17d9c45c838c8b89d63dbc
Elyas <elyassalar8@gmail.com>
2025-09-24 13:50:21 +0200

    demo: showing the button prefix and suffix being removed when setText used

--- src/main/java/org/webforj/views/HomeView.java
+++ src/main/java/org/webforj/views/HomeView.java
@@ -1,9 +1,28 @@
 package org.webforj.views;
 
 import com.webforj.component.Composite;
+import com.webforj.component.button.Button;
 import com.webforj.component.html.elements.Div;
+import com.webforj.component.field.TextField;
 import com.webforj.router.annotation.Route;
 
 @Route("/")
 public class HomeView extends Composite<Div> {
+
+  private final Div self = getBoundComponent();
+
+  public HomeView() {
+    final var button = new Button("click");
+    button.setPrefixComponent(new Div("prefix"));
+    button.setSuffixComponent(new Div("suffix"));
+
+    final var buttonText = new TextField("button text");
+    buttonText.setValue(button.getText());
+    buttonText.addValueChangeListener(event -> {
+      final var value = event.getValue() == null ? "click" : event.getValue();
+      button.setText(value);
+    });
+
+    self.add(buttonText, button);
+  }
 }
