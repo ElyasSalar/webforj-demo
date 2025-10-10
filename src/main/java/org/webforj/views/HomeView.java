25994cfbc4ba39b66c75dc8da4e8835f39e0e4c7
Elyas <elyassalar8@gmail.com>
2025-10-10 21:12:45 +0300

    demo: removing component in slot request

--- src/main/java/org/webforj/views/HomeView.java
+++ src/main/java/org/webforj/views/HomeView.java
@@ -1,9 +1,23 @@
 package org.webforj.views;
 
 import com.webforj.component.Composite;
+import com.webforj.component.button.Button;
 import com.webforj.component.html.elements.Div;
+import com.webforj.component.icons.TablerIcon;
 import com.webforj.router.annotation.Route;
 
 @Route("/")
 public class HomeView extends Composite<Div> {
+
+  private final Div self = getBoundComponent();
+
+  public HomeView() {
+    final var button = new Button("Click me!");
+    button.setPrefixComponent(TablerIcon.create("info"));
+
+    final var removePrefixButton = new Button("remove prefix");
+    removePrefixButton.addClickListener(event -> button.setPrefixComponent(null));
+
+    self.add(button, removePrefixButton);
+  }
 }
